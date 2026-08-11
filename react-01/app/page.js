ef9f60ed478776d18f6d28b029380de7f96ccb46
Heitormeira <heitormeira14@gmail.com>
2026-08-11 11:14:52 +0000

    mudança de titu

--- react-01/app/page.js
+++ react-01/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <div className={styles.intro}>
           <h1>
-            {"Deixa eu dar uma  botadinha LIl "}
+            {"Heitor Meira site"}
             <code className={styles.code}>page.js</code> file.
           </h1>
           <p>
